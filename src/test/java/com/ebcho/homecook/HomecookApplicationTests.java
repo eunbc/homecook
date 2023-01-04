9eeae23b00d5247b4924e506db78c6a3a3a63c78
ebcho <joeunbi514@naver.com>
2023-01-04 20:46:04 +0900

    프로젝트 생성

---
+++ src/test/java/com/ebcho/homecook/HomecookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebcho.homecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

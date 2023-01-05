740688cbd7d77ec81c0f3dc130f706ec67e1558e
ebcho <joeunbi514@naver.com>
2023-01-05 15:05:29 +0900

    post entity, repository 생성

---
+++ src/main/java/com/ebcho/homecook/domain/post/Post.java
@@ -0,0 +1,30 @@
+package com.ebcho.homecook.domain.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private Long cookTime;
+
+    @Builder
+    public Post(String title, String content, Long cookTime) {
+        this.title = title;
+        this.content = content;
+        this.cookTime = cookTime;
+    }
+
+}
